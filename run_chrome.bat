c82a0f7f69780638b210b9f848554d109d076f28
Ramazan <you@example.com>
2020-11-12 10:35:44 +0000

    fix

--- run_chrome.bat
+++ run_chrome.bat
@@ -6,4 +6,8 @@
 set currentdir=%cd::\=/%
 set currentdir=%currentdir:\=/%
 
-docker run -t --rm -v "/%currentdir%:/tmp/source" -w /tmp/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey="05d33cde-849c-4b66-8aeb-07d1e258f30c"
+docker run -t --rm -v "${PWD}:/tmp/source" -w /tmp/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey="05d33cde-849c-4b66-8aeb-07d1e258f30c"
+
+docker run -t --rm -v "${PWD}:/tmp/source" -w /tmp/source katalonstudio/katalon katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey="05d33cde-849c-4b66-8aeb-07d1e258f30c"
+
+docker run -t --rm -v "${PWD}:/tmp/source" -w /tmp/source katalonstudio/katalon katalon-execute.sh -noSplash -runMode=console -retry=0 -testSuiteCollectionPath="Test Suites/TS_RegressionTestCollection" -apiKey="05d33cde-849c-4b66-8aeb-07d1e258f30c"
